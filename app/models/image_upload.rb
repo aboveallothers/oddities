b0d23a78c75f9ad9277c343bde43bd6d052b3387
Dmitri <thewaspfacktory@gmail.com>
2012-12-27 18:27:47 +0000

    hey:

--- app/models/image_upload.rb
+++ app/models/image_upload.rb
@@ -1,9 +1,10 @@
 class ImageUpload < ActiveRecord::Base
 	def self.save(upload)
-	name = upload['imageupload'].original_filename
-	directory = "public/data"
-	# create the file path
-	path = File.join(directory, name)
-	# write the file
-	File.open(path, "wb") { |f| f.write(upload['imageupload'].read) }
+		name = upload['imageupload'].original_filename
+		directory = "public/data"
+		# create the file path
+		path = File.join(directory, name)
+		# write the file
+		File.open(path, "wb") { |f| f.write(upload['imageupload'].read) }
+	end
 end
